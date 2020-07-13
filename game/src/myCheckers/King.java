a19143c259da91d75d6237de38f3b1bf30c8c08d
gabe chavez <gabe.chavez77@gmail.com>
2020-07-13 08:18:05 -1000

    update uper

---
+++ game/src/myCheckers/King.java
@@ -0,0 +1,15 @@
+package myCheckers;
+
+import static myCheckers.Board.piece;
+
+public class King
+{
+
+    // did piece move to King's row? Change type to King
+    if (piece.getType().moveDir == 1 && newY == 0) {
+        piece = makePiece(PieceType.RED_KING, x, y);
+    } else if (piece.getType().moveDir == -1 && newY == 7) {
+        piece = makePiece(PieceType.WHITE_KING, x, y);
+    }
+
+}
